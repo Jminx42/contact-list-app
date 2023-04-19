33e40f501ff301a548b396149112472f42cc6438
Jminx42 <visagiejessica@gmail.com>
2023-04-19 18:15:57 +0000

    added basic layout and some functionality

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,31 @@
-import React from "react";
-import rigoImage from "../../img/rigo-baby.jpg";
-import "../../styles/home.css";
+import React, { useContext } from "react";
+import Contact from "../component/contact";
+import { Context } from "../store/appContext";
+import { Link } from "react-router-dom";
 
-export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
+
+export const Home = () => {
+const {store, actions} = useContext(Context);
+
+	return (
+		<div className="container text-center mt-5">
+			<Link to="/addcontact">
+			<button className="btn btn-primary">Add new contact</button>
+			</Link>
+		
+		<ul className="list-group">
+		{
+			store.contacts.map((contact) => {
+				return <li className="list-group-item" key={contact.id}><Contact  person={contact}/></li>
+			})
+		}
+		</ul>	
+		
+		
 		<a href="#" className="btn btn-success">
 			If you see this green button, bootstrap is working
 		</a>
 	</div>
-);
+	)
+	
+	};
